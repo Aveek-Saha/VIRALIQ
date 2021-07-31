c84b43ff16df048a34a504a452d926d5db93a268
Aveek Saha <aveek.s98@gmail.com>
2021-07-31 12:31:00 +0530

    Add placeholder image

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,6 +3,10 @@
 
 <div class="container">
     <div class="row">
+        <div class="col-8">One of two columns</div>
+        <div class="col-4">
+			<img src="assets/placeholder.jpg" class="img-thumbnail" alt="placeholder">
+		</div>
     </div>
 </div>
 
