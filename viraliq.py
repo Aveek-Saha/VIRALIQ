8ec927ce8bc2461c6ac64aed3e7c94d739b88720
Aveek Saha <aveek.s98@gmail.com>
2021-08-13 21:03:22 +0530

    Add cli options

--- viraliq.py
+++ viraliq.py
@@ -1,6 +1,36 @@
 import click
+from database_clustering import *
+
+@click.group()
+@click.version_option()
+def cli():
+    """VIRALIQ.
+    This is a command line program to search for videos using an image query. 
+    First create embeddings for your videos and then search them quickly
+    using an image.
+    """
 
 @click.command()
-def cli():
-    """Example script."""
-    click.echo('Hello World!')
+@click.option("cluster_type",
+              '--database',
+              default=True,
+              flag_value="database",
+              help="Cluster entire video database")
+@click.option("cluster_type",
+              '--video',
+              flag_value="video",
+              help="Cluster individual videos")
+@click.argument("dir", type=click.Path(exists=True))
+def cluster(cluster_type, dir):
+    click.echo(cluster_type)
+    click.echo(click.format_filename(dir))
+
+    
+
+@click.command()
+@click.argument("img", type=click.Path(exists=True))
+def search(img):
+    click.echo(click.format_filename(img))
+
+cli.add_command(cluster)
+cli.add_command(search)
