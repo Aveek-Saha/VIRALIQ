e51cdfff7af8f4c71dee506b62e61ee819f794b2
Aveek Saha <aveek.s98@gmail.com>
2021-08-16 01:48:46 +0530

    Add config

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='viraliq',
     version='0.1.0',
+    author="Aveek Saha",
+    author_email="aveek.s98@gmail.com",
+    description="Search for videos using an image query.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages= setuptools.find_packages(),
+    include_package_data=True,
     py_modules=['viraliq'],
     install_requires=[
         'Click',
